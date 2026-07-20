4013cfd2fe96321c6e0475e95c81b11aa6109b6c
dehypnotic <jegrmeg@outlook.com>
2026-07-20 21:55:42 +0200

    Lagt til alle nodene inkl i __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,34 @@
 from .nodes.range_to_string import RangeToString
 from .nodes.set_get import DehypnoticSetNode, DehypnoticGetNode
+from .nodes.aspect_ratio_advanced_v2 import AspectRatioAdvancedV2
+from .nodes.save_audio_mp3 import SaveAudioMP3
+from .nodes.save_images import SaveImages
+from .nodes.save_video import SaveVideo
+from .nodes.numbered_text import NumberedText
 
 NODE_CLASS_MAPPINGS = {
     "dehypnotic_RangeToString": RangeToString,
     "DehypnoticSetNode": DehypnoticSetNode,
-    "DehypnoticGetNode": DehypnoticGetNode
+    "DehypnoticGetNode": DehypnoticGetNode,
+    "AspectRatioAdvanced": AspectRatioAdvancedV2,
+    "dehypnotic_AspectRatio": AspectRatioAdvancedV2,
+    "SaveAudioMP3Dehypnotic": SaveAudioMP3,
+    "SaveImagesDehypnotic": SaveImages,
+    "SaveVideoDehypnotic": SaveVideo,
+    "NumberedText": NumberedText,
+    "dehypnotic_NumberedText": NumberedText
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "dehypnotic_RangeToString": "🧘 RangeToString (Dehypnotic)",
     "DehypnoticSetNode": "🧘 Set Dehypnotic",
-    "DehypnoticGetNode": "🧘 Get Dehypnotic"
+    "DehypnoticGetNode": "🧘 Get Dehypnotic",
+    "AspectRatioAdvanced": "AspectRatioAdvanced - DEPRECATED - REPLACE",
+    "dehypnotic_AspectRatio": "🧘 AspectRatio (Dehypnotic)",
+    "SaveAudioMP3Dehypnotic": "🧘 Save MP3 (Dehypnotic)",
+    "SaveImagesDehypnotic": "🧘 Save Images (Dehypnotic)",
+    "SaveVideoDehypnotic": "🧘 Save Video & Frames (Dehypnotic)",
+    "NumberedText": "NumberedText - DEPRECATED - REPLACE",
+    "dehypnotic_NumberedText": "🧘 NumberedText (Dehypnotic)"
 }
 WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
